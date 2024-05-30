7c4f72667fb7f2c9cbfc24cd6986cf90e2aa315d
MihaelaO29 <opriamihaela22@yahoo.com>
2024-05-30 14:07:40 +0300

    added the base of the application and style

--- src/App.js
+++ src/App.js
@@ -1,23 +1,74 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import { motion, AnimatePresence } from "framer-motion"
+import exit from './images/exit.png';
+
 
 function App() {
+  const [selectedItem, setSelectedItem] = useState(null)
+  const items = [
+    {
+      id: 'image_1',
+      image: 'https://i.ibb.co/RPPy5Pk/img1.jpg',
+    },
+    {
+      id: 'image_2',
+      image: 'https://i.ibb.co/ngNjBSj/img2.jpg',
+    },
+    {
+      id: 'image_3',
+      image: 'https://i.ibb.co/MMvJpDj/img3.jpg',
+    },
+    {
+      id: 'image_4',
+      image: 'https://i.ibb.co/YBZRRcz/img4.jpg',
+    }
+  ]
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div className='gallery'>
+        {items.map(item => (
+          <motion.div
+            style={{
+              backgroundImage: `url(${item.image})`,
+              backgroundPosition: 'center',
+              backgroundRepeat: 'no-repeat',
+              backgroundSize: 'cover',
+            }}
+            className={item.id + ' image'}
+            layoutId={item.id}
+            onClick={() => setSelectedItem(item)}
+          >
+          </motion.div>
+
+        ))}
+        <AnimatePresence>
+          {selectedItem && (
+            <motion.div className='container'>
+              <motion.div
+                className='preview'
+                layoutId={selectedItem.id}
+                style={{
+                  backgroundImage: `url(${selectedItem.image})`,
+                  backgroundPosition: 'center',
+                  backgroundRepeat: 'no-repeat',
+                  backgroundSize: 'cover',
+                }}
+              >
+                <motion.img
+                  className='close_item'
+                  src={exit}
+                  alt="exit"
+                  onClick={() => setSelectedItem(null)}
+                />
+              </motion.div>
+            </motion.div>
+          )}
+        </AnimatePresence>
+      </div>
+      <div className='text'><p>Tap to open image</p></div>
     </div>
   );
 }
